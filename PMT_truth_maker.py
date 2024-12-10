e987c31c5c8945aa30b974e7ac33146ebef446fc
Cyan <cyan@fend05.cluster>
2024-12-10 15:24:34 +0100

    added debugging file for PMTfication, modularised truth maker

--- PMT_truth_maker.py
+++ PMT_truth_maker.py
@@ -4,7 +4,6 @@ import sqlite3 as sql
 from typing import List, Dict
 from pyarrow.compute import SetLookupOptions
 
-
 class PMTTruthMaker:
     _SCHEMA = pa.schema([
             ('event_no', pa.int32()),
@@ -16,28 +15,78 @@ class PMTTruthMaker:
             ('energy', pa.float32()),
             ('azimuth', pa.float32()),
             ('zenith', pa.float32()),
-            ('pid', pa.int32())
+            ('pid', pa.int32()),
+            # ('azimuth_HE_daughter', pa.float32()),
+            # ('zenith_HE_daughter', pa.float32()),
+            # ('energy_HE_daughter', pa.float32()),
+            # ('pos_x_HE_daughter', pa.float32()),
+            # ('pos_y_HE_daughter', pa.float32()),
+            # ('pos_z_HE_daughter', pa.float32()),
         ])
 
-    def __init__(self, con_source: sql.Connection, source_table: str, truth_table_name: str, event_no_subset: List[int]) -> None:
+    def __init__(self, 
+                 con_source: sql.Connection, 
+                 source_table: str, 
+                 truth_table_name: str, 
+                 HE_dauther_table_name: str,
+                 event_no_subset: List[int]) -> None:
         self.con_source = con_source
         self.source_table = source_table
         self.truth_table_name = truth_table_name
+        self.HE_dauther_table_name = HE_dauther_table_name
         self.event_no_subset = event_no_subset
+        self.receipt_pa = self._build_receipt_pa()
+    
+    def _build_receipt_pa(self) -> pa.Table:
+        receipt_data = {
+            'event_no': self.event_no_subset,
+            'subdirectory_no': [0] * len(self.event_no_subset),
+            'part_no': [0] * len(self.event_no_subset),
+            'shard_no': [0] * len(self.event_no_subset)
+        }
+        return pa.Table.from_pydict(receipt_data)
         
     def __call__(self, subdirectory_no: int, part_no: int, shard_no: int) -> pa.Table:
         return self._get_truth_pa_shard(subdirectory_no, part_no, shard_no)
-        
+    
     def _get_truth_pa_shard(self, subdirectory_no: int, part_no: int, shard_no: int) -> pa.Table:
-        receipt_data = {
-            'event_no': self.event_no_subset,
-            'subdirectory_no': [subdirectory_no] * len(self.event_no_subset),
-            'part_no': [part_no] * len(self.event_no_subset),
-            'shard_no': [shard_no] * len(self.event_no_subset)
+        truth_query = self._build_truth_query()
+        rows, columns = self._execute_query(truth_query)
+        truth_table = self._create_truth_pa_table(rows, columns)
+        matched_truth_table = self._filter_rows(truth_table)
+        return self._merge_tables(matched_truth_table, subdirectory_no, part_no, shard_no)
+    
+    def _merge_tables(self, matched_truth_table: pa.Table, subdirectory_no: int, part_no: int, shard_no: int) -> pa.Table:
+        merged_data = {
+            'event_no': matched_truth_table['event_no'],
+            'subdirectory_no': pa.array([subdirectory_no] * len(matched_truth_table)),
+            'part_no': pa.array([part_no] * len(matched_truth_table)),
+            'shard_no': pa.array([shard_no] * len(matched_truth_table)),
+            **{col: matched_truth_table[col] for col in matched_truth_table.column_names if col not in ['event_no']}
         }
-        receipt_pa = pa.Table.from_pydict(receipt_data)
+        return pa.Table.from_pydict(merged_data, schema=PMTTruthMaker._SCHEMA)
 
-        # SQL Query
+    def _filter_rows(self, table: pa.Table) -> pa.Table:
+        event_no_column_truth_list = table['event_no'].to_pylist()
+        event_no_column_receipt_list = self.receipt_pa['event_no'].to_pylist()
+
+        if not event_no_column_truth_list or not event_no_column_receipt_list:
+            return pa.Table.from_pydict({field.name: [] for field in PMTTruthMaker._SCHEMA}, schema=PMTTruthMaker._SCHEMA)
+
+        lookup_options = SetLookupOptions(value_set=pa.array(event_no_column_receipt_list))
+        filtered_rows = pc.is_in(pa.array(event_no_column_truth_list), options=lookup_options)
+        return table.filter(filtered_rows)
+    
+    def _create_truth_pa_table(self, rows: List[tuple], columns: List[str]) -> pa.Table:
+        if not rows:
+            return pa.Table.from_pydict({field.name: [] for field in PMTTruthMaker._SCHEMA}, schema=PMTTruthMaker._SCHEMA)
+        
+        truth_data = {col: [row[i] for row in rows] for i, col in enumerate(columns)}
+        truth_data['offset'] = pc.cumulative_sum(pa.array(truth_data['N_doms']))
+        return pa.Table.from_pydict(truth_data)
+    
+    def _build_truth_query(self) -> str:
+        """Construct the SQL query for the truth table."""
         event_filter = ','.join(map(str, self.event_no_subset))
         truth_query = f"""
             SELECT t.event_no, t.energy, t.azimuth, t.zenith, t.pid,
@@ -47,34 +96,9 @@ class PMTTruthMaker:
             WHERE t.event_no IN ({event_filter})
             GROUP BY t.event_no
         """
+        return truth_query
+    
+    def _execute_query(self, query: str) -> List[Dict]:
         cursor = self.con_source.cursor()
-        cursor.execute(truth_query)
-        rows = cursor.fetchall()
-        if not rows:
-            return pa.Table.from_pydict({field.name: [] for field in PMTTruthMaker._SCHEMA}, schema=PMTTruthMaker._SCHEMA)
-
-        columns = [desc[0] for desc in cursor.description]
-        truth_data = {col: [row[i] for row in rows] for i, col in enumerate(columns)}
-        truth_data['offset'] = pc.cumulative_sum(pa.array(truth_data['N_doms']))
-        truth_table = pa.Table.from_pydict(truth_data)
-
-        event_no_column_truth_list = truth_table['event_no'].to_pylist()
-        event_no_column_receipt_list = receipt_pa['event_no'].to_pylist()
-
-        if not event_no_column_truth_list or not event_no_column_receipt_list:
-            return pa.Table.from_pydict({field.name: [] for field in PMTTruthMaker._SCHEMA}, schema=PMTTruthMaker._SCHEMA)
-
-        lookup_options = SetLookupOptions(value_set=pa.array(event_no_column_receipt_list))
-        filtered_rows = pc.is_in(pa.array(event_no_column_truth_list), options=lookup_options)
-        matched_truth_table = truth_table.filter(filtered_rows)
-
-        merged_data = {
-            'event_no': matched_truth_table['event_no'],
-            'subdirectory_no': pa.array([subdirectory_no] * len(matched_truth_table)),
-            'part_no': pa.array([part_no] * len(matched_truth_table)),
-            'shard_no': pa.array([shard_no] * len(matched_truth_table)),
-            **{col: matched_truth_table[col] for col in truth_table.column_names if col not in ['event_no']}
-        }
-
-        return pa.Table.from_pydict(merged_data, schema=PMTTruthMaker._SCHEMA)
-
+        cursor.execute(query)
+        return cursor.fetchall(), [desc[0] for desc in cursor.description]
--- PMTfier.py
+++ PMTfier.py
@@ -20,12 +20,22 @@ from PMT_truth_maker import PMTTruthMaker
 
 class PMTfier:
     def __init__(self, 
-                source_table: str, 
+                source_root: str,
+                source_subdirectory: str,
+                source_table: str,
                 dest_root: str, 
                 N_events_per_shard: int) -> None:
+        self.source_root = source_root
+        self.source_subdirectory = source_subdirectory
         self.source_table = source_table
         self.dest_root = dest_root
         self.N_events_per_shard = N_events_per_shard
+        
+        source_dir = os.path.join(self.source_root, self.source_subdirectory)
+        # first file in the destination root is used to obtain the truth table name
+        first_db_file = os.path.join(source_dir, next((f for f in os.listdir(source_dir) if f.endswith('.db')), None))
+        self.truth_table_name = self._get_truth_table_name_db(first_db_file)
+        self.HE_dauther_table_name = "GNHighestEDaughter" # table name for the highest energy daughter particle
         
         self.signal_or_noise_name = os.path.basename(os.path.normpath(self.dest_root))
         if self.signal_or_noise_name not in ['Snowstorm', 'Corsika']:
@@ -55,8 +65,9 @@ class PMTfier:
                 break
             last_event_no = batch[-1]
             yield batch
-
-    def _get_truth_table_name_db(self, con_source: sql.Connection) -> str:
+    
+    def _get_truth_table_name_db(self, source_file:str) -> str:
+        con_source = sql.connect(source_file)
         cur_source = con_source.cursor()
         cur_source.execute("SELECT name FROM sqlite_master WHERE type='table'")
         tables = [row[0] for row in cur_source.fetchall()]
@@ -66,7 +77,20 @@ class PMTfier:
             truth_table = 'Truth'
         else:
             raise ValueError("Neither 'truth' nor 'Truth' table exists in the source database.")
+        con_source.close()
         return truth_table
+    
+    # def _get_truth_table_name_db(self, con_source: sql.Connection) -> str:
+    #     cur_source = con_source.cursor()
+    #     cur_source.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    #     tables = [row[0] for row in cur_source.fetchall()]
+    #     if 'truth' in tables:
+    #         truth_table = 'truth'
+    #     elif 'Truth' in tables:
+    #         truth_table = 'Truth'
+    #     else:
+    #         raise ValueError("Neither 'truth' nor 'Truth' table exists in the source database.")
+    #     return truth_table
     
     def _get_subdir_tag(self, subdir: str) -> int:
         if self.signal_or_noise_name == 'Snowstorm':
@@ -78,17 +102,15 @@ class PMTfier:
             range_start = subdir.split('-')[0]  # "0002000"
             range_end = subdir.split('-')[1]  # "0002999"
             
-            # Extracting digits from the respective ranges
             first_half_subdir_no = range_start[3:5]  # "20"
             second_half_subdir_no = range_end[3:5]  # "29"
             
-            # Combine the two parts into a single integer
             combined_subdir_no = int(first_half_subdir_no + second_half_subdir_no)
             return combined_subdir_no
             
     def _get_part_no(self, file: str) -> int:
         """
-        file (str): get the number of the file (e.g., "merged_part_1.db").
+        file (str): get the number of the file (e.g., 1 for "merged_part_1.db").
         """
         try:
             file_no = int(file.split('_')[-1].split('.')[0])
@@ -138,21 +160,18 @@ class PMTfier:
         
     def pmtfy_shard(self, 
                     con_source: sql.Connection,
-                    source_table: str,
-                    truth_table_name: str,
-                    source_subdirectory: str,
                     part_no: int,
                     shard_no: int,
                     event_batch: List[int]) -> pa.Table:
-        subdir_tag = self._get_subdir_tag(source_subdirectory)
-        dest_dir = os.path.join(self.dest_root, source_subdirectory, str(part_no))
+        subdir_tag = self._get_subdir_tag(self.source_subdirectory)
+        dest_dir = os.path.join(self.dest_root, self.source_subdirectory, str(part_no))
         os.makedirs(dest_dir, exist_ok=True)
         
         # NOTE
         # PMTSummariser is the core class to be called for PMTfication
         summariser  = PMTSummariser(
             con_source=con_source,
-            source_table=source_table,
+            source_table=self.source_table,
             event_no_subset=event_batch)
         
         pa_pmtfied = summariser()
@@ -162,31 +181,24 @@ class PMTfier:
         
         # NOTE
         # PMT truth table for this shard is created by PMTTruthMaker and returned
-        truth_maker = PMTTruthMaker(con_source, source_table, truth_table_name, event_batch)
+        truth_maker = PMTTruthMaker(con_source, self.source_table, self.truth_table_name, self.HE_dauther_table_name, event_batch)
         pa_truth_shard = truth_maker(int(subdir_tag), part_no, shard_no)
         pa_truth_shard = self._add_enhance_event_no(pa_truth_shard, subdir_tag, part_no)
         
         return pa_truth_shard
         
     def _divide_and_conquer_part(self, 
-                            con_source: sql.Connection, 
-                            source_table: str, 
-                            truth_table_name: str,
-                            source_subdirectory: str, 
-                            part_no: int, 
-                            N_events_per_shard: int) -> pa.Table:
+                            con_source: sql.Connection,
+                            part_no: int) -> pa.Table:
         """
         Divides the database events into shards and processes each shard, consolidating the truth+receipt data.
         """
         truth_shards = []
         
-        for shard_no, event_batch in enumerate(self._get_event_no_batches(con_source, source_table, N_events_per_shard), start=1):
-            logging.info(f"Processing shard {shard_no} of part {part_no} in subdirectory {source_subdirectory}.")
+        for shard_no, event_batch in enumerate(self._get_event_no_batches(con_source, self.source_table, self.N_events_per_shard), start=1):
+            logging.info(f"Processing shard {shard_no} of part {part_no} in subdirectory {self.source_subdirectory}.")
             pa_truth_shard = self.pmtfy_shard(
                 con_source=con_source,
-                source_table=source_table,
-                truth_table_name=truth_table_name,
-                source_subdirectory=source_subdirectory,
                 part_no=part_no,
                 shard_no=shard_no,
                 event_batch=event_batch)
@@ -195,111 +207,45 @@ class PMTfier:
         consolidated_truth = pa.concat_tables(truth_shards)
         return consolidated_truth
         
-    def pmtfy_part(self,
-                source_subdirectory: str,
-                source_part_file: str,
-                source_table: str,
-                N_events_per_shard: int) -> None:
+    def pmtfy_part(self, 
+                   source_part_file: str,
+                   ) -> None:
         part_no = self._get_part_no(source_part_file)
         con_source = sql.connect(source_part_file)
-        truth_table_name = self._get_truth_table_name_db(con_source)
+        # truth_table_name = self._get_truth_table_name_db(con_source)
         
         consolidated_truth = self._divide_and_conquer_part(
             con_source=con_source,
-            source_table=source_table,
-            truth_table_name=truth_table_name,
-            source_subdirectory=source_subdirectory,
-            part_no=part_no,
-            N_events_per_shard=N_events_per_shard)
+            part_no=part_no)
 
-        dest_subdirectory_path = os.path.join(self.dest_root, source_subdirectory)
+        dest_subdirectory_path = os.path.join(self.dest_root, self.source_subdirectory)
         os.makedirs(dest_subdirectory_path, exist_ok=True)
         consolidated_file = os.path.join(dest_subdirectory_path, f"truth_{part_no}.parquet")
         pq.write_table(consolidated_truth, consolidated_file)
 
         con_source.close()
     
-    def pmtfy_subdir_parallel(self, 
-                            source_root: str, 
-                            subdirectory_name: str, 
-                            source_table: str, 
-                            N_events_per_shard: int = 2000, 
-                            max_workers: int = 5) -> None:
+    def pmtfy_subdir_parallel(self, max_workers: int = 5) -> None:
         """
         Processes each database file in a specific subdirectory using parallel workers 
         and saves results in a mirrored directory structure.
         """
-        subdirectory_path = os.path.join(source_root, subdirectory_name)
-        if os.path.isdir(subdirectory_path) and subdirectory_name.isdigit():
-            file_parts = [f for f in os.listdir(subdirectory_path) if f.endswith('.db') and os.path.isfile(os.path.join(subdirectory_path, f))]
-            logging.info(f"Found {len(file_parts)} database files in subdirectory {subdirectory_name}.")
-            if max_workers is None:
-                max_workers = min(len(file_parts), cpu_count() // 2)
-            
-            with ProcessPoolExecutor(max_workers=max_workers) as executor:
-                futures = {
-                executor.submit(self.pmtfy_part, 
-                                source_subdirectory=subdirectory_name,
-                                source_part_file=os.path.join(subdirectory_path, file_part),
-                                source_table=source_table,
-                                N_events_per_shard=N_events_per_shard): file_part
-                for file_part in file_parts
-            }
-                for future in tqdm(as_completed(futures), desc=f"Processing {subdirectory_name}", total=len(file_parts)):
-                    filename = futures[future]
-                    try:
-                        future.result()
-                        logging.info(f"Finished processing database file: {filename}")
-                    except Exception as e:
-                        logging.error(f"Error processing file {filename}: {str(e)}", exc_info=True)
-                    
-def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    logging.info("PMTfication starts...")
-    
-    # User-defined constants
-    source_root = "/lustre/hpc/project/icecube/HE_Nu_Aske_Oct2024/sqlite_pulses/Snowstorm/"
-    dest_root = "/lustre/hpc/project/icecube/HE_Nu_Aske_Oct2024/PMTfied/Snowstorm/"
-    source_table_name = 'SRTInIcePulses'
-    
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="PMTfication of SQLite databases into Parquet files.")
-    parser.add_argument("subdirectory_in_number", type=str, help="Name of the source subdirectory.")
-    parser.add_argument("N_events_per_shard", type=int, help="Number of events per shard.")
-    args = parser.parse_args()
-    
-    # Logging the subdirectory being processed
-    subdirectory_path = os.path.join(source_root, args.subdirectory_in_number)
-    try:
-        files_count = len(os.listdir(subdirectory_path))
-        logging.info(f"The number of files in the subdirectory: {files_count}")
-    except FileNotFoundError as e:
-        logging.error(f"Directory not found: {e}")
-        sys.exit(1)
-    
-    max_workers = int(getenv('SLURM_CPUS_PER_TASK', '1'))  # Default to 1 if undefined
-    logging.info(f"Using up to {max_workers} workers.")
-    
-    # PMTfication logic
-    N_events_per_shard = args.N_events_per_shard
-    pmtfier = PMTfier(source_table_name, dest_root, N_events_per_shard)
-    pmtfier.pmtfy_subdir_parallel(
-        source_root=source_root,
-        subdirectory_name=args.subdirectory_in_number,
-        source_table=source_table_name,
-        N_events_per_shard=N_events_per_shard,
-        max_workers=max_workers
-    )
-    logging.info("PMTfication completed.")
-
-# Wrap the main function in a try-except block
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        logging.error(f"An error occurred: {e}", exc_info=True)
-        sys.exit(1)
+        subdirectory_path = os.path.join(self.source_root, self.source_subdirectory)
+        file_parts = [f for f in os.listdir(subdirectory_path) if f.endswith('.db') and os.path.isfile(os.path.join(subdirectory_path, f))]
+        logging.info(f"Found {len(file_parts)} database files in subdirectory {self.source_subdirectory}.")
+        if max_workers is None:
+            max_workers = min(len(file_parts), cpu_count() // 2)
+        
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = {
+            executor.submit(self.pmtfy_part, 
+                            source_part_file=os.path.join(subdirectory_path, file_part)): file_part
+            for file_part in file_parts
+        }
+            for future in tqdm(as_completed(futures), desc=f"Processing {self.source_subdirectory}", total=len(file_parts)):
+                filename = futures[future]
+                try:
+                    future.result()
+                    logging.info(f"Finished processing database file: {filename}")
+                except Exception as e:
+                    logging.error(f"Error processing file {filename}: {str(e)}", exc_info=True)
