47277572391a5b21919a4336121fbaad89b4a04b
KUcyans <cyan.jo@proton.me>
2025-05-04 23:45:14 +0200

    efficient db reading for pulsemap statview

--- StatView_aesthetic/PulseMapFeatureStatView.py
+++ StatView_aesthetic/PulseMapFeatureStatView.py
@@ -30,6 +30,7 @@ class PulseMapFeatureStatViewer:
                     energy_range: EnergyRange,
                     flavour: Flavour,
                     event_indices: list[int],
+                    N_pulses_cut: int,
                     ):
         setMplParam()
         self.source_root = source_root
@@ -37,10 +38,12 @@ class PulseMapFeatureStatViewer:
         self.N_events = N_events
         self.energy_range = energy_range
         self.flavour = flavour
-        self.event_indices = event_indices
-        
         flavour_to_colour = {Flavour.MU: 0, Flavour.TAU: 1, Flavour.E: 2}
         self.colour_i = flavour_to_colour.get(flavour)
+        
+        self.event_indices = event_indices
+        self.N_pulses_cut = N_pulses_cut
+        
         self.this_df = self._get_df_from_first_part(source_root, energy_range, flavour, N_events)
         
     def __call__(self):
@@ -65,7 +68,7 @@ class PulseMapFeatureStatViewer:
                     plt.close(fig)
 
                 # Charge vs Time per DOM (can return multiple figures)
-                fig_list = self.plot_charge_time_single_event_by_DOM(event_id)
+                fig_list = self.plot_charge_time_single_event_by_DOM(event_id, N_pulses_cut=self.N_pulses_cut)
                 for f in fig_list:
                     pdf.savefig(f)
                     plt.close(f)
@@ -81,19 +84,35 @@ class PulseMapFeatureStatViewer:
             feature_dict.setdefault(event_no, {})[(string, dom)] = values
         return feature_dict
 
-    def _convertDBtoDF(self, file: str, table: str, N_events: int = None) -> pd.DataFrame:
+    # def _convertDBtoDF(self, file: str, table: str, N_events: int = None) -> pd.DataFrame:
+    #     con = sql.connect(file)
+    #     print(f"Reading {N_events} events from {table} of {file}")
+    #     event_no_query = f"SELECT DISTINCT event_no FROM {table} LIMIT {N_events}"
+    #     event_nos = pd.read_sql_query(event_no_query, con)["event_no"].tolist()
+
+    #     event_no_list = ', '.join(map(str, event_nos))
+    #     query = f"SELECT * FROM {table} WHERE event_no IN ({event_no_list})"
+
+    #     df = pd.read_sql_query(query, con)
+    #     con.close()
+
+    #     return df
+    
+    def _convertDBtoDF_iterative(self, file: str, table: str, N_events: int) -> pd.DataFrame:
         con = sql.connect(file)
-        print(f"Reading {N_events} events from {table} of {file}")
         event_no_query = f"SELECT DISTINCT event_no FROM {table} LIMIT {N_events}"
         event_nos = pd.read_sql_query(event_no_query, con)["event_no"].tolist()
 
-        event_no_list = ', '.join(map(str, event_nos))
-        query = f"SELECT * FROM {table} WHERE event_no IN ({event_no_list})"
-
-        df = pd.read_sql_query(query, con)
+        dfs = []
+        for batch_start in tqdm(range(0, len(event_nos), 100), desc="Reading DB in batches"):
+            batch_event_nos = event_nos[batch_start:batch_start+100]
+            placeholders = ','.join('?' for _ in batch_event_nos)
+            query = f"SELECT * FROM {table} WHERE event_no IN ({placeholders})"
+            df_batch = pd.read_sql_query(query, con, params=batch_event_nos)
+            dfs.append(df_batch)
         con.close()
+        return pd.concat(dfs, ignore_index=True)
 
-        return df
     
     def _get_first_db_part_file(self, source_root:str, energy_range: EnergyRange, flavour: Flavour) -> str:
         subdir = EnergyRange.get_subdir(energy_range, flavour)
@@ -102,9 +121,8 @@ class PulseMapFeatureStatViewer:
     
     def _get_df_from_first_part(self, source_root:str, energy_range: EnergyRange, flavour: Flavour, N_events: int) -> pd.DataFrame:
         db_file = self._get_first_db_part_file(source_root, energy_range, flavour)
-        df = self._convertDBtoDF(db_file, 'SRTInIcePulses', N_events=N_events)
+        df = self._convertDBtoDF_iterative(db_file, 'SRTInIcePulses', N_events=N_events)
         return df
-    
     
     ## plotter functions
     def plot_N_active_DOMs_per_event(self) -> matplotlib.figure.Figure:
@@ -134,9 +152,11 @@ class PulseMapFeatureStatViewer:
         ax.axvline(x=256, color=getColour(5), linestyle=':', linewidth=2, label='256 DOMs')
 
         # Plot formatting
-        ax.set_title(fr"Distribution of active DOMs per event for ${self.flavour.latex}$ in {self.energy_range.latex} ({self.N_events} events)")
-        ax.set_xlabel("Number of active DOMs per event")
-        ax.set_ylabel("Number of events")
+        ax.set_title(fr"Distribution of active DOMs per event "
+            fr"for ${self.flavour.latex}$ in {self.energy_range.latex} ({self.N_events} events)", fontsize=22)
+
+        ax.set_xlabel("Number of active DOMs per event", fontsize=20)
+        ax.set_ylabel("Number of events", fontsize=20)
 
         d = {
             'N events': self.N_events,
@@ -150,7 +170,7 @@ class PulseMapFeatureStatViewer:
             'binwidth': binwidth,
             'Nbins': Nbins,
         }
-        add_text_to_ax(0.70, 0.85, nice_string_output(d), ax, fontsize=14)
+        add_text_to_ax(0.60, 0.85, nice_string_output(d), ax, fontsize=18)
         ax.legend(fontsize=14)
         plt.tight_layout()
         return fig
@@ -161,7 +181,7 @@ class PulseMapFeatureStatViewer:
         All DOMs with at least one pulse are included.
         """
 
-        charge_dict = self.get_feature_by_events(self.this_df, 'charge')
+        charge_dict = self._get_feature_by_events(self.this_df, 'charge')
 
         try:
             event_no = list(charge_dict.keys())[event_index]
@@ -181,9 +201,9 @@ class PulseMapFeatureStatViewer:
         ax.hist(counts, bins=bins, color=getColour(self.colour_i), histtype='step', linewidth=2)
 
         ax.set_title(fr"Distribution of pulses across DOMs in event {event_no} "
-                    fr"(${self.flavour.latex}$, {self.energy_range.latex})", fontsize=24)
-        ax.set_xlabel("Number of pulses per DOM", fontsize=16)
-        ax.set_ylabel("Number of DOMs", fontsize=16)
+                    fr"(${self.flavour.latex}$, {self.energy_range.latex})", fontsize=22)
+        ax.set_xlabel("Number of pulses per DOM", fontsize=20)
+        ax.set_ylabel("Number of DOMs", fontsize=20)
 
         # Annotate
         d = {
@@ -198,10 +218,9 @@ class PulseMapFeatureStatViewer:
             'Nbins': Nbins,
         }
 
-        add_text_to_ax(0.68, 0.85, nice_string_output(d), ax, fontsize=14)
+        add_text_to_ax(0.60, 0.85, nice_string_output(d), ax, fontsize=18)
 
         plt.tight_layout()
-        plt.show()
         return fig
 
     def plot_pulse_count_distribution_multi_event(self) -> matplotlib.figure.Figure:
@@ -209,7 +228,7 @@ class PulseMapFeatureStatViewer:
         Plot distribution of number of pulses per DOM across multiple events.
         All DOMs with at least one pulse are included.
         """
-        charge_dict = self.get_feature_by_events(self.this_df, 'charge')
+        charge_dict = self._get_feature_by_events(self.this_df, 'charge')
         
         # Collect pulse counts
         pulse_counts = []
@@ -229,12 +248,12 @@ class PulseMapFeatureStatViewer:
 
         # Plot
         fig, ax = plt.subplots(figsize=(17, 11))
-        ax.hist(counts, bins=bins, color=getColour(colour_i), histtype='step', linewidth=2)
+        ax.hist(counts, bins=bins, color=getColour(self.colour_i), histtype='step', linewidth=2)
 
         ax.set_title(fr"Distribution of pulses across DOMs in {self.N_events} events "
-                    fr"(${self.flavour.latex}$, {self.energy_range.latex})", fontsize=24)
-        ax.set_xlabel("Number of pulses per DOM", fontsize=16)
-        ax.set_ylabel("Number of DOMs", fontsize=16)
+                    fr"(${self.flavour.latex}$, {self.energy_range.latex})", fontsize=22)
+        ax.set_xlabel("Number of pulses per DOM", fontsize=20)
+        ax.set_ylabel("Number of DOMs", fontsize=20)
 
         # Annotate
         d = {
@@ -250,19 +269,18 @@ class PulseMapFeatureStatViewer:
             'Nbins': Nbins,
         }
 
-        add_text_to_ax(0.68, 0.85, nice_string_output(d), ax, fontsize=14)
+        add_text_to_ax(0.60, 0.85, nice_string_output(d), ax, fontsize=18)
 
         plt.tight_layout()
-        plt.show()
         return fig
     
-    def plot_charge_time_single_event_by_DOM(self, event_index: int, N_pulses_cut: int = 100) -> list[matplotlib.figure.Figure]:
+    def plot_charge_time_single_event_by_DOM(self, event_index: int, N_pulses_cut: int) -> list[matplotlib.figure.Figure]:
         """
         Return list of figures showing charge vs time scatter plots for DOMs in a selected event.
         Each figure shows one DOM's hits if the number of pulses exceeds the threshold.
         """
-        charge_dict = self.get_feature_by_events(self.this_df, 'charge')
-        time_dict = self.get_feature_by_events(self.this_df, 'dom_time')
+        charge_dict = self._get_feature_by_events(self.this_df, 'charge')
+        time_dict = self._get_feature_by_events(self.this_df, 'dom_time')
 
         try:
             event_no = list(charge_dict.keys())[event_index]
@@ -271,7 +289,9 @@ class PulseMapFeatureStatViewer:
         except IndexError:
             print(f"Invalid event_index: {event_index}")
             return []
-
+        
+        N_DOMs = len(charge_map)
+        
         fig_list = []
         for (string, dom) in charge_map:
             charge_list = charge_map[(string, dom)]
@@ -279,17 +299,19 @@ class PulseMapFeatureStatViewer:
             if len(charge_list) != len(time_list) or len(charge_list) <= N_pulses_cut:
                 continue
 
-            fig, ax = plt.subplots(figsize=(14, 9))
+            fig, ax = plt.subplots(figsize=(17, 11))
             colour = getColour(self.colour_i)
             ax.vlines(time_list, 0, charge_list, color=colour, linewidth=2, zorder=1)
             ax.scatter(time_list, charge_list, s=30, color=colour, edgecolor='black', linewidth=1, zorder=2)
 
             ax.set_title(fr"Charge vs Arrival Time for (string {string:.0f}, DOM {dom:.0f}) "
-                        fr"in event {event_no} of ${self.flavour.latex}$ in {self.energy_range.latex}")
-            ax.set_xlabel("Arrival time (ns)")
-            ax.set_ylabel("Charge")
+             fr"in event {event_no}({N_DOMs} active DOMs) of "
+             fr"${self.flavour.latex}$ in {self.energy_range.latex}", fontsize=22)
+            ax.set_xlabel("Arrival time (ns)", fontsize=20)
+            ax.set_ylabel("Charge", fontsize=20)
 
             d = {
+                # f'1 of {N_DOMs} active DOMs': "",
                 'N pulses': len(charge_list),
                 'max charge': max(charge_list),
                 'min charge': min(charge_list),
@@ -307,19 +329,19 @@ class PulseMapFeatureStatViewer:
                 '(t4,q4)' : f"({time_list[3]:.0f}, {charge_list[3]:.3f})",
                 '(t5,q5)' : f"({time_list[4]:.0f}, {charge_list[4]:.3f})",
             }
-            T10, T50 = self.get_elapsed_time_until_charge_fraction(charge_list, time_list)
-            Q25, Q75, Qtotal = self.get_accumulated_charge_after_ns(charge_list, time_list)
+            T10, T50 = self._get_elapsed_time_until_charge_fraction(charge_list, time_list)
+            Q25, Q75, Qtotal = self._get_accumulated_charge_after_ns(charge_list, time_list)
             d_quantiles = {
-                r'$T_{10\%}$':f"{T10:.0f}",
-                r'$T_{50\%}$':f"{T50:.0f}",
-                r'$\sigma_T $  ':f"{np.std(time_list):.3f}",
                 r"$Q_{0-25ns}$":f"{Q25:.3f}",
                 r"$Q_{0-75ns}$":f"{Q75:.3f}",
                 r"$Q_{\infty}$" :f"{Qtotal:.3f}",
+                r'$T_{10\%}$':f"{T10:.0f}",
+                r'$T_{50\%}$':f"{T50:.0f}",
+                r'$\sigma_T $  ':f"{np.std(time_list):.3f}",
                 }
-            add_text_to_ax(0.60, 0.95, nice_string_output(d), ax, fontsize=14)
-            add_text_to_ax(0.50, 0.65, nice_string_output(d_first), ax, fontsize=14)
-            add_text_to_ax(0.80, 0.65, nice_string_output(d_quantiles), ax, fontsize=14)
+            add_text_to_ax(0.35, 0.95, nice_string_output(d), ax, fontsize=18)
+            add_text_to_ax(0.70, 0.95, nice_string_output(d_first), ax, fontsize=18)
+            add_text_to_ax(0.70, 0.77, nice_string_output(d_quantiles), ax, fontsize=18)
             plt.tight_layout()
             fig_list.append(fig)
 
@@ -363,20 +385,68 @@ class PulseMapFeatureStatViewer:
 
 if __name__ == "__main__":
     source_root = "/lustre/hpc/project/icecube/HE_Nu_Aske_Oct2024/sqlite_pulses/Snowstorm/"
-    er = EnergyRange.ER_10_TEV_1_PEV
-    flavour = Flavour.E
-    N_events = 1000
-    event_id = {Flavour.E: [7],
-                Flavour.MU: [23], 
-                Flavour.TAU: [16]}
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("N_events", type=int, help="Number of events to process")
+    
+    parser.add_argument("energy_id", type=int, choices=[0, 1, 2],
+                        help="Energy range: 0 = 10TeV–1PeV, 1 = 1PeV–100PeV")
+    parser.add_argument("flavour_id", type=int, choices=[0, 1, 2],
+                        help="Flavour: 0 = nu_e, 1 = nu_mu, 2 = nu_tau")
+    args = parser.parse_args()
+
+    # Integer-to-enum mapping
+    energy_map = {
+        0: EnergyRange.ER_10_TEV_1_PEV,
+        1: EnergyRange.ER_1_PEV_100_PEV,
+        2: EnergyRange.ER_100_TEV_100_PEV
+    }
+    flavour_map = {
+        0: Flavour.E,
+        1: Flavour.MU,
+        2: Flavour.TAU,
+    }
+    N_events = args.N_events
+    er = energy_map[args.energy_id]
+    flavour = flavour_map[args.flavour_id]
+
+    ALL_SETUPS = {
+        EnergyRange.ER_10_TEV_1_PEV: {
+            "pulse_cut": 150,
+            "events": {
+                Flavour.E: [7],
+                Flavour.MU: [23],
+                Flavour.TAU: [16],
+            }
+        },
+        EnergyRange.ER_1_PEV_100_PEV: {
+            "pulse_cut": 250,
+            "events": {
+                Flavour.E: [16],
+                Flavour.MU: [31],
+                Flavour.TAU: [0],
+            }
+        },
+    }
+    SETUP = ALL_SETUPS[er]
+    event_ids = SETUP["events"][flavour]
+    N_pulses_cut = SETUP["pulse_cut"]
+    output_pdf = f"PulseMapFeatures_{er.string}_{flavour.alias}({N_events}events).pdf"
+    
+    print(f"Flavour: {flavour}, Energy Range: {er}, N_events: {N_events}")
+    print(f" energy range string: {er.string}")
+    print(f" flavour alias: {flavour.alias}")
+    print(f"Output PDF: {output_pdf}")
     
     start_time = time.time()
-    viewer = PulseMapFeatureStatViewer(source_root=source_root,
-                                        output_pdf=f"PulseMapFeatures_{er.string}_{flavour.alias}({N_events}events).pdf",
+    PulseMapFeatureStatViewer(source_root=source_root,
+                                        output_pdf=output_pdf,
                                         N_events=N_events,
                                         energy_range=er,
                                         flavour=flavour,
-                                        event_indices=event_id[flavour])
+                                        event_indices=SETUP["events"][flavour],
+                                        N_pulses_cut=SETUP["pulse_cut"],
+                                        )()
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Time taken to complete: {elapsed_time:.2f} seconds")
--- StatView_aesthetic/PulseMapFeatureStatView.sh
+++ StatView_aesthetic/PulseMapFeatureStatView.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 #SBATCH --job-name=pulse_map_feature_stat_view%j
 #SBATCH --partition=icecube
-##SBATCH --nodelist=node[194-194] # node[187-191] or node[194-211]
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=60G
-#SBATCH --time=02:00:00
+#SBATCH --time=10:00:00
 #SBATCH --signal=B:USR1@60
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 
-LOG_DIR="log/${SUBDIR}"
+ENERGY_ID=${ENERGY_ID:-0}   # 0 = 10TeV–1PeV, 1 = 1PeV–100PeV, 2 = 100TeV–100PeV
+FLAVOUR_ID=${FLAVOUR_ID:-0} # 0 = nu_e, 1 = nu_mu, 2 = nu_tau
+N_EVENTS=${N_EVENTS:-10000} # Number of events to process
+
+ENERGY_ID=1
+FLAVOUR_ID=2 
+N_EVENTS=10000
+
+LOG_DIR="log/"
 mkdir -p "${LOG_DIR}"
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
-logfile="${LOG_DIR}/[${timestamp}]FeatureStatView_${SLURM_JOB_ID}.log"
+logfile="${LOG_DIR}/[${timestamp}]FeatureStatView_er${ENERGY_ID}_fl${FLAVOUR_ID}.log"
 
-exec > /dev/null 2> "${logfile}"
+exec > "${logfile}" 2>&1
 
 echo "Starting job at $(date)"
+echo "Energy ID: ${ENERGY_ID}, Flavour ID: ${FLAVOUR_ID}"
 
-python3.9 -u PulseMapFeatureStatView.py 
+python3.9 -u PulseMapFeatureStatView.py "${N_EVENTS}" "${ENERGY_ID}" "${FLAVOUR_ID}"
 
 echo "Job completed at $(date)"
